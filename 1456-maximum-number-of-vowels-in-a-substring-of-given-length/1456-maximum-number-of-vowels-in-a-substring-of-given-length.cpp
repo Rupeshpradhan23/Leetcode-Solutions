fc122b1d74755df90fb27aeb7b59e46c300055e1
Rupesh Pradhan <143020838+Rupeshpradhan23@users.noreply.github.com>
2026-07-02 16:23:39 +0530

    Time: 3 ms (88.48%), Space: 13.1 MB (60.36%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,17 +3,13 @@ public:
     bool isVowel(char ch) {
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
-
     int maxVowels(string s, int k) {
         int count = 0;
-
         for (int i = 0; i < k; i++) {
             if (isVowel(s[i]))
                 count++;
         }
-
         int ans = count;
-
         for (int i = k; i < s.length(); i++) {
             if (isVowel(s[i - k]))
                 count--;
@@ -23,7 +19,6 @@ public:
 
             ans = max(ans, count);
         }
-
         return ans;
     }
 };
